49f1bb75f205e3619d20bee3d67b050517ab4216
pavelusov <ipavelusov@gmail.com>
2018-10-06 14:51:31 +0500

    Define User model

---
+++ models/users.js
@@ -0,0 +1,13 @@
+// User model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define User model
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true, // Unique value support
+    lowercase: true // Save lower case value
+  },
+  password: String
+});
