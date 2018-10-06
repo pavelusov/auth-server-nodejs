90227f19a0fbe6362965953855ae6239654f2007
pavelusov <ipavelusov@gmail.com>
2018-10-06 18:21:49 +0500

    Added save user

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,5 +1,33 @@
+const User = require('../models/User');
+
 exports.signup = (req, res, next) => {
-  res.send({
-    success: true
+  const { body = {} } = req;
+  const { email = '', password = '' } = body;
+
+  // Регистрация
+  // Проверяем есть ли пользователь с таким же логином(email)
+  User.findOne({ email }, (err, existingUser) => {
+    if (err) {
+      return next(err);
+    }
+
+    // Если пользователь с таким email существует, вернуть ошибку
+    if (existingUser) {
+      return res.status(422).send({error: 'Email уже используется'});
+    }
+
+    // Если пользователя с таким email НЕ существует, то создать User и сохранить
+    const user = new User({
+      email,
+      password
+    });
+
+    user.save(err => {
+      if (err) {
+        return next(err);
+      }
+      // Вернуть сообщение что пользователь создан
+      res.status(201).json({message: "Пользователь создан"});
+    });
   })
 };
